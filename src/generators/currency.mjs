e1292f04cde025c1cbfb1dbfae0d7e00744cd9ff
Balazs Horvath <kadelam@hellbound.dog>
2021-01-23 07:23:36 +0100

    currency
    Signed-off-by: Balazs Horvath <kadelam@hellbound.dog>

--- src/generators/currency.mjs
+++ src/generators/currency.mjs
@@ -5,7 +5,7 @@ Array.prototype.random = ArrayRandom;
 
 const start = ['Un', 'El', 'Ta', 'Be'];
 
-const end = ['ve', 'ee', 'li'];
+const end = ['ve', 'ee', 'li', 'coin'];
 
 /**
  * Generates the name of the currency used in the player's world
@@ -14,4 +14,4 @@ function generateCurrency() {
   return start.random() + end.random();
 }
 
-console.log(generateCurrency());
+export {generateCurrency};
